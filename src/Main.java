d84831b2a814881ca27899ee1e14b1c48628df30
EricHafemann <eric_hafemann@estudante.sesisenai.org.br>
2025-07-05 23:50:05 -0300

    V1

---
+++ src/Main.java
@@ -0,0 +1,151 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    static ArrayList<Double> notas = new ArrayList<>();
+    static ArrayList<String> alunos = new ArrayList<>();
+    clearScream cs = new clearScream();
+
+    public static void main(String[] args) {
+        menuInicial();
+    }
+
+    public static void menuInicial() {
+        Scanner s = new Scanner(System.in);
+        int op = 0;
+
+        boolean valido = true;
+        do {
+            try {
+                System.out.println("===========================");
+                System.out.println("==    MENU DO SISTEMA    ==");
+                System.out.println("===========================\n");
+
+                System.out.println("1- CADASTRAR ALUNO");
+                System.out.println("2- VER ALUNOS CADASTRADOS");
+                System.out.println("3- SAIR");
+                System.out.println("===========================");
+                System.out.print("Escolha uma opção: ");
+
+                op = s.nextInt();
+                s.nextLine();
+
+                switch (op) {
+                    case 1:
+                        cadastrarAluno();
+                        break;
+                    case 2:
+                        exibirAlunos();
+                        break;
+                    case 3:
+                        System.out.println("Programa Finalizado!");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("\n\n================================");
+                        System.out.println("Valor inválido. Tente novamente!");
+                        System.out.println("================================");
+                        valido = false;
+                        clearScream.limparTela();
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("\n\n===========================");
+                System.out.println("   Digite apenas números.  ");
+                System.out.println("===========================");
+                clearScream.limparTela();
+                s.nextLine();
+            }
+
+        } while (valido);
+    }
+
+    public static void cadastrarAluno() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
+        String nome = s.nextLine();
+        alunos.add(nome);
+
+        int contNota = 1;
+        double nota = 0;
+
+        do {
+            System.out.println("Digite a " + contNota + "ª nota: ");
+            try {
+                nota = s.nextDouble();
+
+                if (nota >= 0 && nota <= 10) {
+                    notas.add(nota);
+                    contNota++;
+                } else {
+                    System.out.println("Nota Inválida. Somente de 0 a 10.\n");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Digite apenas números.\n");
+                s.nextLine();
+            }
+
+        } while (contNota <= 3);
+
+
+
+
+        System.out.println("Aluno cadastrado com sucesso!\n");
+    }
+
+    public static void exibirAlunos() {
+
+        Scanner s = new Scanner(System.in);
+
+        if (alunos.isEmpty()) {
+            System.out.println("\n==========================");
+            System.out.println("  Nenhum aluno cadastrado.  ");
+            System.out.println("==========================\n");
+
+            System.out.println("Aperte Enter para voltar ao Menu...");
+            String  a1 = s.nextLine();
+
+            clearScream.limparTela();
+            menuInicial();
+
+        } else {
+            System.out.println("Alunos cadastrados:\n");
+
+            int ind = 0;
+            String sit = null;
+            int contAlunos = 1;
+
+            for (String aluno : alunos) {
+
+                System.out.println(contAlunos+"- " + aluno);
+                System.out.println("Notas: ");
+                System.out.println("1- Nota:"+notas.get(ind));
+                System.out.println("2- Nota:"+notas.get(ind + 1));
+                System.out.println("3- Nota:"+notas.get(ind + 2));
+                double md = (notas.get(ind) + notas.get(ind+1) + notas.get(ind+2) ) / 3;
+                System.out.printf("Média: %.2f\n", md);
+
+                if (md >= 7)
+                {
+                    sit = "Aprovado";
+                }
+                else
+                {
+                    sit = "Reprovado";
+                }
+                System.out.println("Situação: "+sit+"\n");
+                ind = ind + 3;
+                contAlunos++;
+
+            }
+            System.out.println("\nAperte Enter para voltar ao Menu...");
+                String  a2 = s.nextLine();
+
+            clearScream.limparTela();
+            menuInicial();
+        }
+    }
+}
